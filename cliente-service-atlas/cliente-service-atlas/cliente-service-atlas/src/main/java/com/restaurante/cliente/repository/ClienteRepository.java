67e0569dd21a5e325f3276a33668ea95535d0438
Levy Porfirio <133019069+LevyPorfirio@users.noreply.github.com>
2025-06-05 13:56:01 -0300

    Delete cliente-service-atlas/cliente-service-atlas/cliente-service-atlas directory

--- cliente-service-atlas/cliente-service-atlas/cliente-service-atlas/src/main/java/com/restaurante/cliente/repository/ClienteRepository.java
+++
@@ -1,6 +0,0 @@
-package com.restaurante.cliente.repository;
-
-import com.restaurante.cliente.model.Cliente;
-import org.springframework.data.mongodb.repository.MongoRepository;
-
-public interface ClienteRepository extends MongoRepository<Cliente, String> {}
