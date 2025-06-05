0e6851ace7154e85371278a0e3408c20814280ab
Levy Porfirio <133019069+LevyPorfirio@users.noreply.github.com>
2025-06-05 20:41:35 -0300

    Add files via upload

---
+++ cliente-service-atlas/src/main/java/com/restaurante/cliente/model/Cliente.java
@@ -0,0 +1,38 @@
+package com.restaurante.cliente.model;
+
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+@Document(collection = "clientes")
+public class Cliente {
+
+    @Id
+    private String id;
+    private String nome;
+    private String email;
+    private String telefone;
+    private String endereco;
+
+    public Cliente() {}
+
+    public Cliente(String nome, String email, String telefone, String endereco) {
+        this.nome = nome;
+        this.email = email;
+        this.telefone = telefone;
+        this.endereco = endereco;
+    }
+
+    public String getId() { return id; }
+
+    public String getNome() { return nome; }
+    public void setNome(String nome) { this.nome = nome; }
+
+    public String getEmail() { return email; }
+    public void setEmail(String email) { this.email = email; }
+
+    public String getTelefone() { return telefone; }
+    public void setTelefone(String telefone) { this.telefone = telefone; }
+
+    public String getEndereco() { return endereco; }
+    public void setEndereco(String endereco) { this.endereco = endereco; }
+}
